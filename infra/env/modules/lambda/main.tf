6965781f21d836c6902befe7ba0825fd91d25cc3
shivajikhot <shivajkv@gmail.com>
2025-02-03 12:29:38 +0530

    Create main.tf

---
+++ infra/env/modules/lambda/main.tf
@@ -0,0 +1,35 @@
+resource "aws_lambda_function" "patient_service" {
+  function_name = "patient-service"
+  role          = var.lambda_execution_role_arn
+  package_type  = "Image"
+  image_uri     = var.patient_service_image_uri
+
+  memory_size = 512
+  timeout     = 30
+  publish     = true
+
+  environment {
+    variables = {
+      NODE_ENV = "production"
+      DB_URL   = var.patient_db_url
+    }
+  }
+}
+
+resource "aws_lambda_function" "appointment_service" {
+  function_name = "appointment-service"
+  role          = var.lambda_execution_role_arn
+  package_type  = "Image"
+  image_uri     = var.appointment_service_image_uri
+
+  memory_size = 512
+  timeout     = 30
+  publish     = true
+
+  environment {
+    variables = {
+      NODE_ENV = "production"
+      DB_URL   = var.appointment_db_url
+    }
+  }
+}
