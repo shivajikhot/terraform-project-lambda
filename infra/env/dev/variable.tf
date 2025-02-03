fcd64f9d17dbca027ba9dc9035a0bb16bb8e1dfb
shivajikhot <shivajkv@gmail.com>
2025-02-03 13:35:14 +0530

    Update variable.tf

--- infra/env/dev/variable.tf
+++ infra/env/dev/variable.tf
@@ -1,10 +1,12 @@
 variable "patient_service_image_uri" {
   description = "ECR image URI for Patient Service"
   type        = string
+  default     = "575108922676.dkr.ecr.us-west-1.amazonaws.com/patient-service-repo:latest"
 }
 
 variable "appointment_service_image_uri" {
   description = "ECR image URI for Appointment Service"
   type        = string
+  default     = "575108922676.dkr.ecr.us-west-1.amazonaws.com/appointment-service-repo:latest"
 }
 
