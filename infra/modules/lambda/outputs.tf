f7490112a0fba9ce6f56342c31672a6891579411
shivajikhot <shivajkv@gmail.com>
2025-02-03 17:44:26 +0530

    Update outputs.tf

--- infra/modules/lambda/outputs.tf
+++ infra/modules/lambda/outputs.tf
@@ -2,3 +2,8 @@ output "hello_world_lambda_arn" {
   value = aws_lambda_function.hello_world.arn
 }
 
+output "function_name" {
+  description = "Name of the Lambda function."
+
+  value = aws_lambda_function.hello_world.function_name
+}
