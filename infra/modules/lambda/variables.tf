f0e462ab31fcb013a12a6096250693d813f1a022
shivajikhot <shivajkv@gmail.com>
2025-02-03 13:04:47 +0530

    Update variables.tf

--- infra/modules/lambda/variables.tf
+++ infra/modules/lambda/variables.tf
@@ -1,5 +1,3 @@
 variable "patient_service_image_uri" {}
 variable "appointment_service_image_uri" {}
-variable "patient_db_url" {}
-variable "appointment_db_url" {}
 variable "lambda_execution_role_arn" {}
