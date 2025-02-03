8deececdab9ab835ecd7e979fc15ebc6962822d1
shivajikhot <shivajkv@gmail.com>
2025-02-03 18:19:31 +0530

    Update variable.tf

--- infra/env/dev/variable.tf
+++ infra/env/dev/variable.tf
@@ -1,4 +1,4 @@
-variable "hello-world_image_uri" {
+variable "hello_world_image_uri" {
   description = "ECR image URI for Patient Service"
   type        = string
   default     = "575108922676.dkr.ecr.us-west-1.amazonaws.com/hello-world:latest"
