c1d8ef6b847a09e11e63e3910f05c0fbed631e87
shivajikhot <shivajkv@gmail.com>
2025-02-03 17:27:59 +0530

    Update mian.tf

--- infra/env/dev/mian.tf
+++ infra/env/dev/mian.tf
@@ -4,15 +4,13 @@ module "iam" {
 
 module "lambda" {
   source                        = "../../modules/lambda"
-  patient_service_image_uri     = var.patient_service_image_uri
-  appointment_service_image_uri = var.appointment_service_image_uri
+  hello_world_image_uri     = var.hello_world_image_uri
   lambda_execution_role_arn     = module.iam.lambda_execution_role_arn
 }
 
 module "apigateway" {
   source                     = "../../modules/apigateway"
-  patient_lambda_arn         = module.lambda.patient_lambda_arn
-  appointment_lambda_arn     = module.lambda.appointment_lambda_arn
+  patient_lambda_arn         = module.lambda.hello_world_lambda_arn
   region                 = "us-west-1"
 }
 module "ecr" {
