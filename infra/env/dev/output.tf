54b38ca20ff504fa52256b60cb09483ca3a67478
shivajikhot <shivajkv@gmail.com>
2025-02-03 19:02:29 +0530

    Update output.tf

--- infra/env/dev/output.tf
+++ infra/env/dev/output.tf
@@ -2,3 +2,7 @@ output "function_name" {
   description = "Base URL of the deployed API Gateway"
   value       = module.lambda.function_name
 }
+output "base_url" {
+  description = "Base URL of the deployed API Gateway"
+  value       = module.apigateway.base_url
+}
