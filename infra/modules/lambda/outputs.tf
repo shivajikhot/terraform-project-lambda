e448d1af4f5e92addf824bbd58c175086fbc3a40
shivajikhot <shivajkv@gmail.com>
2025-02-04 14:02:06 +0530

    Update outputs.tf

--- infra/modules/lambda/outputs.tf
+++ infra/modules/lambda/outputs.tf
@@ -1,7 +1,12 @@
-output "patient_lambda_arn" {
-  value = aws_lambda_function.patient_service.arn
+output "hello_world_lambda_arn" {
+  value = aws_lambda_function.appointment_service.arn
 }
 
-output "appointment_lambda_arn" {
-  value = aws_lambda_function.appointment_service.arn
+output "function_name" {
+  description = "Name of the Lambda function."
+
+  value = aws_lambda_function.appointment_service.function_name
+}
+output "hello_world_invoke_arn" {
+  value = aws_lambda_function.appointment_service.invoke_arn
 }
