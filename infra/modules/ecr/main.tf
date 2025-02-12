ff238ea11d8fe44afb350910343b23376846b7d1
shivajikhot <shivajkv@gmail.com>
2025-02-12 12:23:04 +0530

    Update main.tf

--- infra/modules/ecr/main.tf
+++ infra/modules/ecr/main.tf
@@ -1,14 +1,8 @@
 # ECR Repository for the microservices
-resource "aws_ecr_repository" "patient_service" {
-  name = "patient-service-repo"
+resource "aws_ecr_repository" "hello-world" {
+  name = "hello-world"
   tags = {
-    Name = "Patient Service Repository"
+    Name = "hello-world Repository"
   }
 }
 
-resource "aws_ecr_repository" "appointment_service" {
-  name = "appointment-service-repo"
-  tags = {
-    Name = "Appointment Service Repository"
-  }
-}
