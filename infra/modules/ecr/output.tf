cd81fa9422f850623b8a3b62ce611b30f89bda05
shivajikhot <shivajkv@gmail.com>
2025-02-12 12:23:52 +0530

    Update output.tf

--- infra/modules/ecr/output.tf
+++ infra/modules/ecr/output.tf
@@ -1,8 +1,4 @@
 # Outputs for ECR Repository URIs
-output "patient_service_repo_uri" {
-  value = aws_ecr_repository.patient_service.repository_url
-}
-
-output "appointment_service_repo_uri" {
-  value = aws_ecr_repository.appointment_service.repository_url
+output "hello-world_repo_uri" {
+  value = aws_ecr_repository.hello-world.repository_url
 }
