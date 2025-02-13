8c42b058c7818cc34d6be61840c8ae4d1c9f7ff1
shivajikhot <shivajkv@gmail.com>
2025-02-13 08:30:52 +0530

    Update main.tf

--- infra/modules/apigateway/main.tf
+++ infra/modules/apigateway/main.tf
@@ -18,7 +18,7 @@ resource "aws_apigatewayv2_authorizer" "cognito_auth" {
   api_id          = aws_apigatewayv2_api.lambda.id
   name            = "CognitoAuth"
   authorizer_type = "JWT"
-  identity_sources = "$request.header.Authorization"
+  identity_sources = ["$request.header.Authorization"]
 
   jwt_configuration {
     audience = [aws_cognito_user_pool_client.client.id]
