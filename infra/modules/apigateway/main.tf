4e5ffdebe6fc482d348577fbaa06da94e71b1d30
shivajikhot <shivajkv@gmail.com>
2025-02-14 13:21:02 +0530

    Update main.tf

--- infra/modules/apigateway/main.tf
+++ infra/modules/apigateway/main.tf
@@ -21,7 +21,7 @@ resource "aws_cognito_user_pool_client" "client" {
   allowed_oauth_flows = ["implicit"]
   allowed_oauth_scopes = ["openid"]
   allowed_oauth_flows_user_pool_client = true
-  callback_urls = ["${aws_apigatewayv2_api.lambda.api_endpoint}/serverless_lambda_stage/hello"]  # Update frontend URL
+  callback_urls = ["${aws_api_gateway_rest_api.hello_api.api_endpoint}/dev/hello"]  # Update frontend URL
 }
 
 # Cognito User Pool Domain (For Hosted UI)
