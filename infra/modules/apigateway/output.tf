ece5290593529c8a6b0fb03f8ea926c7875879e6
shivajikhot <shivajkv@gmail.com>
2025-02-14 14:55:29 +0530

    Update output.tf

--- infra/modules/apigateway/output.tf
+++ infra/modules/apigateway/output.tf
@@ -1,5 +1,5 @@
 output "base_url" {
   description = "Base URL for API Gateway stage."
 
-  value = aws_apigatewayv2_stage.lambda.invoke_url
+  value = aws_api_gateway_deployment.deployment.invoke_url
 }
